d46d663aa8c68f58ca272fc37fccc4533a588f3a
VorobiovKir <kvorobiov89@gmail.com>
2016-01-17 22:33:52 +0200

    homework

--- project/twitter_wall/views.py
+++ project/twitter_wall/views.py
@@ -19,6 +19,16 @@ class TwitListView(TemplateView):
     def init_api(self):
         import twitter
 
+        api = twitter.Api(
+            consumer_key='nQ1FbhLREforyjU3rmfE0mOai',
+            consumer_secret='3uaNilLha1dnpRvst6I2n5LcAnXFgH2nobGt74kr909XtJaxeS',
+            # access_token_key='4804017803-e4LQBFQMyCBXcggNRlwEriBVd8vtPtsZdq3G3dh',
+            # access_token_secret='nVRenPJaRIrJiM6iOfFclurGcOjjUMY946ZUZ2ZAPDdLB'      
+            access_token_key='4804084887-4VxxUJhjRtgBbXTA1Vpkqaybjy08DStFjLhrUaI',
+            access_token_secret='CrQMUKVyRscH5V2XmHxXwWdIjuhQaqNIO4Rwnp5euBcCQ'
+        )
+
+        return api
         # access_token = {
         #     "access_token": {
         #         "oauth_token_secret": "nVRenPJaRIrJiM6iOfFclurGcOjjUMY946ZUZ2ZAPDdLB",
@@ -31,43 +41,25 @@ class TwitListView(TemplateView):
 
         # access_token = {"oauth_token_secret": "CrQMUKVyRscH5V2XmHxXwWdIjuhQaqNIO4Rwnp5euBcCQ", "oauth_token": "4804084887-4VxxUJhjRtgBbXTA1Vpkqaybjy08DStFjLhrUaI", "x_auth_expires": "0", "user_id": "4804084887", "screen_name": "MaistrenkoAnton"}
 
-        api = twitter.Api(
+        # api = twitter.Api(
 
-            # consumer_key = 'HZ4Wf9hGIahvbIwxHpVFDo5Au',
-            # consumer_secret = 'TqPu0GQZV3oQcCcjQnmRHhEs8c37BxhVD28bRJtxcgkJpnmcfC',
-            # access_token='4804084887-4VxxUJhjRtgBbXTA1Vpkqaybjy08DStFjLhrUaI',
-            # access_token_secret='CrQMUKVyRscH5V2XmHxXwWdIjuhQaqNIO4Rwnp5euBcCQ',
+        #     # consumer_key = 'HZ4Wf9hGIahvbIwxHpVFDo5Au',
+        #     # consumer_secret = 'TqPu0GQZV3oQcCcjQnmRHhEs8c37BxhVD28bRJtxcgkJpnmcfC',
+        #     # access_token_key='4804084887-4VxxUJhjRtgBbXTA1Vpkqaybjy08DStFjLhrUaI',
+        #     # access_token_secret='CrQMUKVyRscH5V2XmHxXwWdIjuhQaqNIO4Rwnp5euBcCQ',
 
-            # token=access_token.to_string()
-            # -- MY
-            consumer_key='nQ1FbhLREforyjU3rmfE0mOai',
-            consumer_secret='3uaNilLha1dnpRvst6I2n5LcAnXFgH2nobGt74kr909XtJaxeS',
-            # access_token_key='4804017803-e4LQBFQMyCBXcggNRlwEriBVd8vtPtsZdq3G3dh',
-            # access_token_secret='nVRenPJaRIrJiM6iOfFclurGcOjjUMY946ZUZ2ZAPDdLB'
-            access_token='4804084887-4VxxUJhjRtgBbXTA1Vpkqaybjy08DStFjLhrUaI',
-            access_token_secret='CrQMUKVyRscH5V2XmHxXwWdIjuhQaqNIO4Rwnp5euBcCQ',
-        )
+        #     # token=access_token.to_string()
+        #     # -- MY
+        #     consumer_key='nQ1FbhLREforyjU3rmfE0mOai',
+        #     consumer_secret='3uaNilLha1dnpRvst6I2n5LcAnXFgH2nobGt74kr909XtJaxeS',
+        #     # access_token_key='4804017803-e4LQBFQMyCBXcggNRlwEriBVd8vtPtsZdq3G3dh',
+        #     # access_token_secret='nVRenPJaRIrJiM6iOfFclurGcOjjUMY946ZUZ2ZAPDdLB'
+        #     access_token='4804084887-4VxxUJhjRtgBbXTA1Vpkqaybjy08DStFjLhrUaI',
+        #     access_token_secret='CrQMUKVyRscH5V2XmHxXwWdIjuhQaqNIO4Rwnp5euBcCQ',
+        # )
 
-        return api
 
-    def newApi(self):
-        import twitter
-        api = twitter.Api(
-
-            # consumer_key = 'HZ4Wf9hGIahvbIwxHpVFDo5Au',
-            # consumer_secret = 'TqPu0GQZV3oQcCcjQnmRHhEs8c37BxhVD28bRJtxcgkJpnmcfC',
-            # access_token='4804084887-4VxxUJhjRtgBbXTA1Vpkqaybjy08DStFjLhrUaI',
-            # access_token_secret='CrQMUKVyRscH5V2XmHxXwWdIjuhQaqNIO4Rwnp5euBcCQ',
-
-            # token=access_token.to_string()
-            # -- MY
-
-            access_token='4804084887-4VxxUJhjRtgBbXTA1Vpkqaybjy08DStFjLhrUaI',
-            access_token_secret='CrQMUKVyRscH5V2XmHxXwWdIjuhQaqNIO4Rwnp5euBcCQ',
-        )
-
-        return api
-
+    
     def get_context_data(self, **kwargs):
         context = super(TwitListView, self).get_context_data(**kwargs)
         context['tweets'] = self.getTweets()
@@ -76,19 +68,22 @@ class TwitListView(TemplateView):
 
     def getTweets(self):
         tweets = []
+        followers = {}
+        user = {}
         try:
             api = self.init_api()
-            new = self.newApi()
-            items = api(new).GetUserTimeline(screen_name='Kir_LightIT')
+            items = api.GetUserTimeline()
             latest = self.paginated(items, self.page)
             for tweet in latest:
                 status = tweet.text
                 tweet_date = tweet.relative_created_at
                 tweets.append({'status': status, 'date': tweet_date})
+            followers = api.GetFollowers()
+            user = api.VerifyCredentials()
         except:
             tweets.append({'status': 'Something old', 'date': 'about 10 min ago'})
 
-        return {'tweets': tweets}
+        return {'tweets': tweets, 'followers': followers, 'user': user}
 
     def post(self, request):
         print '-------------------'
